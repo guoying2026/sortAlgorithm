42c61d2353e1b5f5f949c10c810eaa22ed1aacc3
guoying2026 <2473548838@qq.com>
2018-08-01 08:40:57 +0800

    快速排序算法

---
+++ bubblesort.php
@@ -0,0 +1,34 @@
+<?php
+//冒泡排序20180801
+$arr = [98,34,67,45,23,68,12,80,39,79];
+
+//交换函数
+function interchange(&$arr,$low,$high){
+	$temp = $arr[$low];
+	$arr[$low] = $arr[$high];
+	$arr[$high] = $temp;
+}
+
+function bubblesort(&$arr)
+{
+	$length = count($arr);
+	$sum = 0;
+	for($i = 0;$i < $length-1;$i++){
+		$swap = false;
+		//计算时间复杂度
+		for($j = 0;$j < $length-$i-1;$j++){
+			if($arr[$j] < $arr[$j+1]){
+				interchange($arr,$j,$j+1);
+				$swap = true;
+			}
+		}
+		$sum += $i*$j;
+		if(true != $swap){
+			break;
+		}
+	}
+	var_dump($sum);
+	var_dump($arr);
+	return $arr;
+}
+bubblesort($arr);
